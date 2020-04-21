8fd9478369b786892bf1b087a1a521ad2dd7a493
Saint-Paul Tinga <saintpaultinga@gmail.com>
2020-04-21 19:12:04 -0400

    added feature-1

--- app/src/main/java/com/tsp/learn/android/androidci/MainActivity.kt
+++ app/src/main/java/com/tsp/learn/android/androidci/MainActivity.kt
@@ -2,11 +2,15 @@ package com.tsp.learn.android.androidci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        display_btn.setOnClickListener {
+            name_lbl.text = user_name.text
+        }
     }
 }
